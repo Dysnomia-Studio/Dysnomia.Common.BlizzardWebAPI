09c391836fd8fd1c97fe20c64211b54859b3705d
Elanis <elanis@hotmail.com>
2021-06-22 19:26:05 +0200

    First working Test :tada: !

---
+++ Dysnomia.Common.BlizzardWebAPI.Test/BaseTestClass.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+
+using Dysnomia.Common.BlizzardWebAPI.Models;
+
+using Microsoft.Extensions.Configuration;
+
+namespace Dysnomia.Common.BlizzardWebAPI.Test {
+	public abstract class BaseTestClass {
+		protected string CLIENT_ID = "";
+		protected string CLIENT_SECRET = "";
+		protected ulong BattleNetAccountId = 0;
+		protected ulong BattleNetProfileId = 0;
+		protected const ulong INVALID_BNETID = 0;
+		protected string REDIRECT_URI = "";
+
+		protected Task<AccessTokenResponse> authCodeFlowToken;
+		protected Task<AccessTokenResponse> clientCredentialFlowToken;
+
+		public BaseTestClass() {
+			var config = new ConfigurationBuilder()
+				.AddUserSecrets<BaseTestClass>()
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+			CLIENT_ID = config["CLIENT_ID"];
+			CLIENT_SECRET = config["CLIENT_SECRET"];
+			BattleNetAccountId = ulong.Parse(config["BattleNetAccountId"]);
+			BattleNetProfileId = ulong.Parse(config["BattleNetProfileId"]);
+			REDIRECT_URI = config["REDIRECT_URI"];
+
+			var tokenQuerier = new TokenQuerier(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, "eu");
+			authCodeFlowToken = tokenQuerier.GetAuthorizationCodeFlow("TODO");
+			clientCredentialFlowToken = tokenQuerier.GetClientCredentialFlow();
+		}
+	}
+}
---
+++ Dysnomia.Common.BlizzardWebAPI.Test/LegacyStarcraftCommunityTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+using Dysnomia.Common.BlizzardWebAPI.Enums;
+
+using Xunit;
+
+namespace Dysnomia.Common.BlizzardWebAPI.Test {
+	public class LegacyStarcraftCommunityTest : BaseTestClass {
+		protected readonly LegacyStarcraftCommunity legacyStarcraftCommunity;
+
+		public LegacyStarcraftCommunityTest() {
+			this.legacyStarcraftCommunity = new LegacyStarcraftCommunity();
+		}
+
+		[Fact]
+		public async Task GetPlayerProfile_OK() {
+			var token = (await this.clientCredentialFlowToken).access_token;
+
+			var res = await legacyStarcraftCommunity.GetPlayerProfile(token, RegionEnum.EU.ToString(), (int)StarCraft2RegionEnum.Europe, (int)StarCraft2RealmEnum.Europe, BattleNetProfileId);
+
+			Assert.True(res != null);
+		}
+	}
+}
