2c4dedb503cda080dac51a9820702287e3a2a872
Elanis <elanis@hotmail.com>
2023-08-27 17:56:19 +0200

    Re-enable a test for an API that has been fixed

--- Dysnomia.Common.BlizzardWebAPI.Test/Starcraft2CommunityTest.cs
+++ Dysnomia.Common.BlizzardWebAPI.Test/Starcraft2CommunityTest.cs
@@ -6,82 +6,82 @@ using System.Threading.Tasks;
 using Xunit;
 
 namespace Dysnomia.Common.BlizzardWebAPI.Test {
-	public class Starcraft2CommunityTest : BaseTestClass {
-		protected readonly IStarcraft2Community starcraftCommunity;
+    public class Starcraft2CommunityTest : BaseTestClass {
+        protected readonly IStarcraft2Community starcraftCommunity;
 
-		public Starcraft2CommunityTest(IHttpClientFactory httpClientFactory, IStarcraft2Community starcraftCommunity) : base(httpClientFactory) {
-			this.starcraftCommunity = starcraftCommunity;
-		}
+        public Starcraft2CommunityTest(IHttpClientFactory httpClientFactory, IStarcraft2Community starcraftCommunity) : base(httpClientFactory) {
+            this.starcraftCommunity = starcraftCommunity;
+        }
 
-		[Fact]
-		public async Task GetStaticProfile_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetStaticProfile_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetStaticProfile(token, RegionEnum.EU, StarCraft2RegionEnum.Europe);
-			Assert.True(res != null);
-		}
+            var res = await starcraftCommunity.GetStaticProfile(token, RegionEnum.EU, StarCraft2RegionEnum.Europe);
+            Assert.True(res != null);
+        }
 
-		[Fact]
-		public async Task GetMetadataProfile_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetMetadataProfile_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetMetadataProfile(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId);
+            var res = await starcraftCommunity.GetMetadataProfile(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId);
 
-			Assert.True(res != null);
-		}
+            Assert.True(res != null);
+        }
 
-		[Fact]
-		public async Task GetProfile_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetProfile_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetProfile(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId);
+            var res = await starcraftCommunity.GetProfile(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId);
 
-			Assert.True(res != null);
-		}
+            Assert.True(res != null);
+        }
 
-		[Fact]
-		public async Task GetLadderSummary_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetLadderSummary_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetProfileLadderSummary(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId);
+            var res = await starcraftCommunity.GetProfileLadderSummary(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId);
 
-			Assert.True(res != null);
-		}
+            Assert.True(res != null);
+        }
 
-		[Fact]
-		public async Task GetProfileLadder_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetProfileLadder_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetProfileLadder(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId, 0);
+            var res = await starcraftCommunity.GetProfileLadder(token, RegionEnum.EU, StarCraft2RegionEnum.Europe, StarCraft2RealmEnum.Europe, BattleNetProfileId, 0);
 
-			Assert.True(res != null);
-		}
+            Assert.True(res != null);
+        }
 
-		[Fact(Skip = "Server issue")]
-		public async Task GetGrandmasterLeaderboard_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetGrandmasterLeaderboard_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetGrandmasterLeaderboard(token, RegionEnum.EU, StarCraft2RegionEnum.Europe);
+            var res = await starcraftCommunity.GetGrandmasterLeaderboard(token, RegionEnum.EU, StarCraft2RegionEnum.Europe);
 
-			Assert.True(res != null);
-		}
+            Assert.True(res != null);
+        }
 
-		[Fact]
-		public async Task GetLadderSeason_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetLadderSeason_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetLadderSeason(token, RegionEnum.EU, StarCraft2RegionEnum.Europe);
+            var res = await starcraftCommunity.GetLadderSeason(token, RegionEnum.EU, StarCraft2RegionEnum.Europe);
 
-			Assert.True(res != null);
-		}
+            Assert.True(res != null);
+        }
 
-		[Fact]
-		public async Task GetPlayerAccount_OK() {
-			var token = (await this.clientCredentialFlowToken).access_token;
+        [Fact]
+        public async Task GetPlayerAccount_OK() {
+            var token = (await this.clientCredentialFlowToken).access_token;
 
-			var res = await starcraftCommunity.GetPlayerAccount(token, RegionEnum.EU, BattleNetAccountId);
+            var res = await starcraftCommunity.GetPlayerAccount(token, RegionEnum.EU, BattleNetAccountId);
 
-			Assert.True(res != null);
-		}
-	}
+            Assert.True(res != null);
+        }
+    }
 }
